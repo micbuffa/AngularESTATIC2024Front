c01112b96543baa71b8af8a35d75eb875d1d8309
Michel Buffa <michelbuffa@macbook-pro-de-michel.home>
2024-04-10 15:54:41 +0200

    Avec exemples boutons material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
